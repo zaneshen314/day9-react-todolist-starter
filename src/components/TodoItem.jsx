b0621310393f1ee7d27699a7b6ea257249cce98a
SHENZA <shenza@oocl.com>
2024-11-28 20:31:33 +0800

    [todo]feat,add click again to make it done/undone

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,20 @@
+import "./TodoItem.css";
+import {TodoContext} from "../App";
+import {useContext} from "react";
+
 
 const TodoItem = ({todo}) => {
-  return (
-      <div>
-        <div>{todo.text}</div>
-      </div>
-  );
+    const {dispatch} = useContext(TodoContext);
+
+    const handleToggleTodo = () => {
+        dispatch({type: "TOGGLE_TODO", payload: todo.id});
+    };
+
+    return (
+        <div className={"todo-item"}>
+            <div className={`todo-text ${todo.done ? "done" : ""}`} onClick={handleToggleTodo}>{todo.text}</div>
+        </div>
+    );
 }
 
 export default TodoItem;
--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,12 +1,16 @@
 export const initialState = [
   {id: Date.now(), text: "the first todo", done: false},
-  {id: Date.now(), text: "the second todo", done: false},
+  {id: Date.now() + 1, text: "the second todo", done: false},
 ];
 
 export const todoReducer = (state, action) => {
   switch (action.type) {
     case "ADD":
       return [...state, { id: Date.now(), text: action.payload, done: false }];
+    case "TOGGLE_TODO":
+      return state.map((todo) =>
+          todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      );
     default:
       return state;
   }
