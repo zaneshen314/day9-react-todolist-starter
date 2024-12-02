b0c3eabe42a506a941a7b04e8b3a8637e9f2febe
SHENZA <shenza@oocl.com>
2024-12-02 11:14:35 +0800

    [todo]feat,feat NotFound css

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,18 @@
+import React from 'react';
+import './css/NotFound.css';
+
 const TodoList = () => {
-  return (
-      <div>
-          <h1>404 Not Found</h1>
-      </div>
-  );
+    return (
+        <div className="not-found-container">
+            <div className="not-found-content">
+                <h1 className="not-found-title">404</h1>
+                <p className="not-found-message">Oops! The page you're looking for could not be found.</p>
+                <button className="go-home-button" onClick={() => window.location.href = '/'}>
+                    Go to Home
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default TodoList
+export default TodoList;
