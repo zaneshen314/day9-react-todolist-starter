7ac40835a3bcb08814e567570641b95836884038
SHENZA <shenza@oocl.com>
2024-12-02 21:40:51 +0800

    [todo]feat,add interceptor.js and adjust todo.js

---
+++ src/api/interceptor.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "http://localhost:8080"
+})
+
+instance.interceptors.request.use(
+    config => {
+        config.startTime = Date.now();
+        console.log('Request', config);
+        return config;
+    },
+    error => {
+        console.log('Request Error', error);
+        return Promise.reject(error);
+    }
+);
+
+// 响应拦截器
+instance.interceptors.response.use(
+    response => {
+        const endTime = Date.now();
+        const duration = endTime - response.config.startTime;
+        console.log('Response', response);
+        console.log(`Request took ${duration}ms`);
+        return response;
+    },
+    error => {
+        if (error.response && error.response.status === 404) {    window.location.href = "/not-found";}
+        if (error.response && error.response.status === 500) {    window.location.href = "/hard-stop";}
+        const endTime = Date.now();
+        const duration = endTime - error.config.startTime;
+        console.log('Response Error', error);
+        console.log(`Request took ${duration}ms`);
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
+
--- src/api/todo.js
+++ src/api/todo.js
@@ -1,17 +1,14 @@
-import axios from "axios";
+import instance from "./interceptor";
 
-const instance = axios.create({
-    baseURL: "https://67495bf6868020296630a6e3.mockapi.io/"
-})
 
 export const getTodoList = async () =>{
-    const  response = await instance.get("/todo");
+    const  response = await instance.get("/todos");
     return response.data;
 }
 
 export const addTodo = async (newTodo) => {
     try {
-        const response = await instance.post("/todo", newTodo);
+        const response = await instance.post("/todos", newTodo);
         return response.data;
     } catch (error) {
         console.error('添加 Todo 失败:', error);
@@ -21,7 +18,7 @@ export const addTodo = async (newTodo) => {
 
 export const deleteById = async (id) => {
     try {
-        const response = await instance.delete(`/todo/${id}`);
+        const response = await instance.delete(`/todos/${id}`);
         return response.data;
     } catch (error) {
         console.error(`删除 Todo ${id} 失败:`, error);
@@ -31,7 +28,7 @@ export const deleteById = async (id) => {
 
 export const update = async (newTodo) => {
     try {
-        const response = await instance.put(`/todo/${newTodo.id}`,newTodo);
+        const response = await instance.put(`/todos/${newTodo.id}`,newTodo);
         return response.data;
     } catch (error) {
         console.error('更新 Todo 失败:', error);
