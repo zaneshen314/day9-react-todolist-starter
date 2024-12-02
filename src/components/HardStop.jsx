d879565788380990a1d8f39181f9fc28a2373bd9
SHENZA <shenza@oocl.com>
2024-12-02 13:55:12 +0800

    [todo]fix,fix bug HardStop.jsx redirect HOME

--- src/components/HardStop.jsx
+++ src/components/HardStop.jsx
@@ -7,7 +7,7 @@ const TodoList = () => {
             <div className="not-found-content">
                 <h1 className="not-found-title">500</h1>
                 <p className="not-found-message">Server Error,please try again later</p>
-                <button className="go-home-button" onClick={() => window.location.href = '/hard-stop'}>
+                <button className="go-home-button" onClick={() => window.location.href = '/'}>
                     Go to Home
                 </button>
             </div>
